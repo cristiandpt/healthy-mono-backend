ac48444f5c53fd9761f02cb9e147e5c41201431d
Cristian David <cristian.pacheco@correounivalle.edu.co>
2025-07-06 20:06:51 -0500

    Adding the login and auth endpoints to app

---
+++ infrastructure/api/src/main/kotlin/com/cristiandpt/api/config/AuthEndpointsProperties.kt
@@ -0,0 +1,14 @@
+package com.cristiandpt.api.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "api.auth")
+data class AuthENdpointsProperties(
+    var login: String = "",
+    var loginFacebook: String = "",
+    var loginGoogle: String = "",
+    var socialRegister: String = "",
+    var register: String = ""
+)
